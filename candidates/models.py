f699876d9ae4d77c080d6d6a2acf8756a933cad8
Eddie Kao <eddie@5xcampus.com>
2024-02-06 11:39:34 +0800

    add Candidate model

--- candidates/models.py
+++ candidates/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Candidate(models.Model):
+    name = models.CharField(max_length=100)
+    party = models.CharField(max_length=50)
+    age = models.IntegerField()
+    introduction = models.TextField()
