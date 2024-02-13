9b911f8c8ade886f37b654093d0eb8b498ffa44c
Eddie Kao <eddie@5xcampus.com>
2024-02-13 10:50:25 +0800

    add candidate

--- candidates/views.py
+++ candidates/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from .models import Candidate
 
 
 def index(request):
+    if request.method == "POST":
+        candidate = Candidate(
+            name=request.POST["name"],
+            party=request.POST["party"],
+            age=request.POST["age"],
+            introduction=request.POST["introduction"],
+        )
+        candidate.save()
+        return redirect("candidates:index")
+
     return render(request, "candidates/index.html")
 
 
